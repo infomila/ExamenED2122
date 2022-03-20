3b482c5a5f5e5313d9370c4796b534ccbeeb569b
Bernat <bernat.orellana@gmail.com>
2022-03-20 07:50:01 +0100

    Bug #76 Solved

--- ClientEditor/src/clienteditor/EmailValidator.java
+++ ClientEditor/src/clienteditor/EmailValidator.java
@@ -39,6 +39,7 @@ import org.jdesktop.beansbinding.Validator;
 public class EmailValidator extends Validator<String> {
 
     public Validator.Result validate(String arg) {
+	  if(args==null) return null;
         if ((arg.length() < 4) || !arg.contains("@") || !arg.contains(".")) {
             return new Result(null, "Please enter a valid email");
         }
